1336c9b31e0031ef722d69e3bcdea83ef0c2e4ce
tumikole <ramollosamuel2@gmail.com>
2021-09-14 16:16:51 +0200

    adding home page css

--- src/Components/form.js
+++ src/Components/form.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./nav";
-import Save from '../static/SAVE.svg'
+import Save from "../static/SAVE.svg";
 import "../CSS/form.css";
 class form extends Component {
   constructor(props) {
@@ -31,138 +31,140 @@ class form extends Component {
               { name: "Edit", location: "/Edit" },
             ]}
           />
-
         </div>
-<br />
+        <br />
         <hr />
 
-        <h1 style={{ color:'red', textAlign: "center" }}>Add Employee</h1>
+        <h1 style={{ color: "red", textAlign: "center" }}>Add Employee</h1>
         <div className="mainForm">
-          <br/>
-        <form >
-          First name:
-          <input
-            class="form-control"
-            style={{
-              borderColor: name.length < 6 ? "red" : "green",
-            }}
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.props.handleChange}
-          />
           <br />
-          Last name:
-          <input
-            style={{
-              borderColor: surname.length < 6 ? "red" : "green",
-            }}
-            class="form-control"
-            type="text"
-            name="surname"
-            value={surname}
-            onChange={this.props.handleChange}
-          />
-          <br />
-          Programming Language:
-          <select
-            style={{
-              borderColor: position.length < 6 ? "red" : "green",
-            }}
-            class="form-control"
-            name="language"
-            value={language}
-            onChange={this.props.handleChange}
-          >
-            <option></option>
-            <option>Javascript</option>
-            <option>Python</option>
-            <option>C#</option>
-            <option>C++</option>
-            <option>Java</option>
-            <option>Visual Basic</option>
-            <option>PHP</option>
-            <option>SQL</option>
-            <option>Assembly</option>
-            <option>Groovy</option>
-          </select>
-          <br />
-          Position:
-          <select
-            style={{
-              borderColor: position.length < 6 ? "red" : "green",
-            }}
-            class="form-control"
-            name="position"
-            value={position}
-            onChange={this.props.handleChange}
-          >
-            <option></option>
-            <option>Senior Developer</option>
-            <option>Junior Developer</option>
-          </select>
-          <br />
-          Task:
-          <input
-            style={{
-              borderColor: task.length < 6 ? "red" : "green",
-            }}
-            class="form-control"
-            type="text"
-            name="task"
-            value={task}
-            onChange={this.props.handleChange}
-          />
-          <br />
-          Salary:
-          <input
-            style={{
-              borderColor: salary.length < 3 ? "red" : "green",
-            }}
-            class="form-control"
-            type="number"
-            name="salary"
-            value={salary}
-            onChange={this.props.handleChange}
-          />
-          <br />
-          Date:
-          <input
-            style={{
-              borderColor: date.length < 6 ? "red" : "green",
-            }}
-            class="form-control"
-            type="date"
-            name="date"
-            value={date}
-            onChange={this.props.handleChange}
-          />
-          <br />
-          <input
-            checked={isSelectAll}
-            name="checkboxAll"
-            onChange={(e) => this.props.handleCheckboxAll(e)}
-            type="checkbox"
-          />
-          {foodChoices.map((choice) => (
-            <div>
-              <label> {choice.name} </label>
-              <input
-                checked={choice.status}
-                name={choice.name}
-                onChange={this.props.handleCheckboxChange}
-                type="checkbox"
+          <form>
+            <input
+              class="form-control"
+              style={{
+                borderColor: name.length < 6 ? "red" : "green",
+              }}
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.props.handleChange}
+              placeholder="Enter your first name"
+            />
+            <br />
+            <input
+              style={{
+                borderColor: surname.length < 6 ? "red" : "green",
+              }}
+              class="form-control"
+              type="text"
+              name="surname"
+              value={surname}
+              onChange={this.props.handleChange}
+              placeholder="Enter your last name"
+            />
+            <br />
+            <select
+              style={{
+                borderColor: position.length < 6 ? "red" : "green",
+              }}
+              class="form-control"
+              name="language"
+              value={language}
+              onChange={this.props.handleChange}
+            >
+              <option>Select Programming Language</option>
+              <option>Javascript</option>
+              <option>Python</option>
+              <option>C#</option>
+              <option>C++</option>
+              <option>Java</option>
+              <option>Visual Basic</option>
+              <option>PHP</option>
+              <option>SQL</option>
+              <option>Assembly</option>
+              <option>Groovy</option>
+            </select>
+            <br />
+            
+            <select
+              style={{
+                borderColor: position.length < 6 ? "red" : "green",
+              }}
+              class="form-control"
+              name="position"
+              value={position}
+              onChange={this.props.handleChange}
+            >
+              <option>Select Position</option>
+              <option>Senior Developer</option>
+              <option>Junior Developer</option>
+            </select>
+            <br />
+            
+            <input
+              style={{
+                borderColor: task.length < 6 ? "red" : "green",
+              }}
+              class="form-control"
+              type="text"
+              name="task"
+              value={task}
+              onChange={this.props.handleChange}
+              placeholder="Task"
+            />
+            <br />
+            
+            <input
+              style={{
+                borderColor: salary.length < 3 ? "red" : "green",
+              }}
+              class="form-control"
+              type="number"
+              name="salary"
+              value={salary}
+              onChange={this.props.handleChange}
+              placeholder="Salary"
+            />
+            <br />
+            <input
+              style={{
+                borderColor: date.length < 6 ? "red" : "green",
+              }}
+              class="form-control"
+              type="date"
+              name="date"
+              value={date}
+              onChange={this.props.handleChange}
+            />
+            <br />
+            <input
+              checked={isSelectAll}
+              name="checkboxAll"
+              onChange={(e) => this.props.handleCheckboxAll(e)}
+              type="checkbox"
+            />
+            {foodChoices.map((choice) => (
+              <div>
+                <label> {choice.name} </label>
+                <input
+                  checked={choice.status}
+                  name={choice.name}
+                  onChange={this.props.handleCheckboxChange}
+                  type="checkbox"
+                />
+              </div>
+            ))}
+            <br />
+            <div className="edit-container">
+              <img
+                className="svg1"
+                src={Save}
+                alt=""
+                onClick={this.props.handleSubmit}
               />
             </div>
-          ))}
+          </form>
           <br />
-         
-          <div className="edit-container">
-                      <img className="svg1" src={Save} alt="" onClick={this.props.handleSubmit}/>
-                    </div>
-                   
-        </form>
-        <br/>
         </div>
       </div>
     );
